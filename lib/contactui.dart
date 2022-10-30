d7d22342ef295e8d381a3d4da99d761c603c0760
sathar123 <satharbest99@gmail.com>
2022-10-30 22:06:41 +0530

    commit

--- lib/contactui.dart
+++ lib/contactui.dart
@@ -23,6 +23,7 @@ class ContactUI extends StatelessWidget {
                 child: Icon(Icons.people,size: 40,color: Colors.white,),
               ),
               title: Text('Person ${index+1}',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+              subtitle: Text('+91 985623781${index}',style: TextStyle(fontSize: 15),),
               trailing: Wrap(
                 children: [
                   Icon(Icons.sms,color: Colors.blue,),
